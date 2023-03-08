df67a3062a7b837ea763e171485adea9ff66e54d
Egor <golderandlomove@gmail.com>
2023-03-08 14:32:52 +0500

    Чек

--- scripts/scrollup.js
+++ scripts/scrollup.js
@@ -2,7 +2,6 @@
 
 const scrollBar = document.querySelector('.scrooll-up');
 const startScroll = document.documentElement.clientHeight * 0.8;
-console.log(screenY);
 
 window.addEventListener('scroll', () => {
   scrollY > startScroll ? (scrollBar.style.visibility = 'visible') : (scrollBar.style.visibility = 'hidden');
