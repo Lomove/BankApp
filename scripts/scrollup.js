06cb765aac5bd6c20ecaaa89da72ee8b384fe7cf
Egor <golderandlomove@gmail.com>
2023-03-10 23:45:11 +0500

    Стики меню (кривое) + фикс окна куки.

--- scripts/scrollup.js
+++ scripts/scrollup.js
@@ -1,8 +1,15 @@
 'use strict';
 
 const scrollBar = document.querySelector('.scrooll-up');
-const startScroll = document.documentElement.clientHeight * 0.8;
+const header = document.querySelector('header');
+const startScroll = document.querySelector('hr').getBoundingClientRect().y * 0.8;
 
 window.addEventListener('scroll', () => {
-  scrollY > startScroll ? scrollBar.classList.add('active') : scrollBar.classList.remove('active');
+  if (scrollY > startScroll) {
+    scrollBar.classList.add('active');
+    header.classList.add('active');
+  } else {
+    scrollBar.classList.remove('active');
+    header.classList.remove('active');
+  }
 });
